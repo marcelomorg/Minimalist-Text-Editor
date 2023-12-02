fd54134a09dcafe1203d187a665dd3d2cbba46ab
Marcelo Morgado <01101101.mgd@gmail.com>
2023-12-02 17:54:13 +0000

    'NORMAL' and 'INSERT' mode  created

--- mte.cpp
+++ mte.cpp
@@ -5,6 +5,8 @@
 Mte::Mte(const std::string& file)
 {
 	textLineCaptured.push_back("");
+	way ='n';
+	status = "NORMAL";
 
 	if (file.empty())
 	{
@@ -19,21 +21,29 @@ Mte::Mte(const std::string& file)
 	noecho();
 	cbreak();
 	keypad(stdscr, true);
-}
+};
 
 Mte::~Mte()
 {
 	refresh();
 	endwin();
-}
+};
 
 void Mte::run()
 {
-	//printw(&filename[0]);
-	//getch();
-	
+	while(way != 'q')
+	{
+		Mte::choose();
+		Mte::statusline();
+		Mte::print();
+		int caracter  = getch();
+		Mte::inputText(caracter);
+	};
+
+	/*
 	while(getmaxyx(stdscr, LINES, COLS))
 	{
+		
 		int intCaptured = getch();
 		std::string caracterCaptured(1, static_cast<char>(intCaptured));
 		textLineCaptured.push_back(caracterCaptured);
@@ -43,4 +53,83 @@ void Mte::run()
 			mvprintw(0, i, textLineCaptured[i].c_str());
 		}
 	}
+	*/ 
+
+	/*
+	printw(&filename[0]);
+	getch();
+	*/ 	
+};
+
+void Mte::choose()
+{
+	switch (way) 
+	{
+		case 27: //KEY "Esc"
+		case 'n':
+			status = "NORMAL";
+			break;
+		case 'i':
+			status = "INSERT";
+			break;
+		case 'q':
+			break;
+	};
+}
+
+void Mte::statusline()
+{
+	attron(A_REVERSE);
+	mvprintw(LINES -1, COLS - (status.length() + 3) , status.c_str() );
+	attroff(A_REVERSE);
+}
+
+void Mte::inputText(int ctr)
+{
+	switch (way)
+	{
+		case 27:
+		case 'n':
+			switch (ctr)
+			{
+				case 'q':
+					way = 'q';
+					break;
+				case 'i':
+					way = 'i';
+					break;
+			}
+			break;
+		case 'i':
+			switch (ctr)
+			{
+				case 27:
+					way = 'n';
+				default:
+					std::string s(1, ctr);
+					//textLineCaptured.push_back(s);
+					textLineCaptured[y].insert(x, 1, ctr);
+					++x;
+					break;
+			}
+	}
+}
+
+void Mte::print()
+{
+	for(size_t i {}; i < static_cast<size_t>(LINES - 1); ++i)
+	{
+		if (i >= textLineCaptured.size())
+		{
+			move(i, 0);
+			clrtoeol();
+		}
+		else
+		{
+			mvprintw(0, i, textLineCaptured[i].c_str());
+
+		}
+		clrtoeol();
+	}	
+	move(y,x); // move cursor of the final from 'Y' to 'X' axis begin.
 }
--- mte.hpp
+++ mte.hpp
@@ -1,18 +1,24 @@
 #pragma once
-#include <vector>
 #pragma clang diagnostic ignored "-Wformat-security"
-
+#include <vector>
 #include <ncurses.h>
 #include <memory>
 #include <string>
 
 class Mte 
 {
+	protected: char x,y;
+	protected: char way;
 	protected: std::string filename;
+	protected: std::string status;
 	protected: std::vector<std::string> textLineCaptured;
 
 	public:
 		Mte(const std::string&);
 		~Mte();
-		void run();
+		void run(); 			//Do loop
+		void choose();			//Choose way
+		void statusline();		//Print screen way status
+		void inputText(int);
+		void print();
 };
